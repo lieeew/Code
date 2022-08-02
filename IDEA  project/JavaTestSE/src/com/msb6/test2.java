2ce0ac783c71c8a5e202ee47d240366df78e6296
lieeew <3530464951@qq.com>
2022-08-02 09:12:19 +0800

    java

--- IDEA  project/JavaTestSE/src/com/msb6/test2.java
+++ IDEA  project/JavaTestSE/src/com/msb6/test2.java
@@ -9,6 +9,9 @@ public class test2 {
     // 属性
     int age;
     static int sid = 10;
+    {
+        System.out.println("------------b");
+    }
     // 空构造器
     public test2 () {
         System.out.println("这个空构造器");
@@ -23,15 +26,18 @@ public class test2 {
     public void eat () {
         System.out.println(sid);
         System.out.println(age);
+        System.out.println("这是一个方法");
     }
     public static void play () {
-        System.out.println(sid);
-    }
 
+        System.out.println(sid);
+        System.out.println("这是一个静态的方法");
+    }
 
     public static void main(String[] args) {
         test2 t = new test2 (10);
-
         System.out.println(t.age);
+        t.eat();
+        t.play();
     }
 }
