0ae04665c223c745a2b84d93ba1faa15cf061b55
lieeew <3530464951@qq.com>
2022-08-02 18:27:47 +0800

    java学习打卡

---
+++ IDEA  project/JavaTestSE/src/EXERCISE/Gril.java
@@ -0,0 +1,26 @@
+package EXERCISE;
+
+/**
+ * @Auther: qiuxinyu
+ * @Date: 2022/8/2 - 08 - 02
+ * @Description: EXERCISE
+ */
+public class Gril {
+    private int age;
+    // 空构造器
+    public Gril () {
+
+    }
+    // 设置年龄
+    public void shezhiAge (int age) {
+        if (age >= 30) {
+            this.age = 18;
+        }else {
+            this.age = age;
+        }
+    }
+    // 读取年龄
+    public int  duquAge () {
+        return age;
+    }
+}
