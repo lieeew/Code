d364ba9a94f4c187e93f2408b504e5981f971f8a
lieeew <3530464951@qq.com>
2022-08-09 12:28:11 +0800

    java

---
+++ IDEA  project/SGG/com.sbg/DAY08/Exercise/StudentTest.java
@@ -0,0 +1,17 @@
+package DAY08.Exercise;
+
+/**
+ * @Auther: qiuxinyu
+ * @Date: 2022/8/9 - 08 - 09
+ * @Description: DAY08.Exercise
+ */
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.age = 18;
+        s.name = "lili";
+        s.sex = 2;
+        System.out.println(s.addAge(2));
+        System.out.println("age:" + s.age + );
+    }
+}
