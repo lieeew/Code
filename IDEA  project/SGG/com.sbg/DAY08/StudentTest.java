e04c61ae3ef4680366d0ecf6e938bf7ec3ce396c
lieeew <3530464951@qq.com>
2022-08-10 23:55:50 +0800

    java

--- IDEA  project/SGG/com.sbg/DAY08/StudentTest.java
+++ IDEA  project/SGG/com.sbg/DAY08/StudentTest.java
@@ -1,8 +1,6 @@
 package DAY08;
 
 
-import java.util.Arrays;
-
 /**
  * @Auther: qiuxinyu
  * @Date: 2022/8/10 - 08 - 10
@@ -15,18 +13,32 @@ public class StudentTest {
            stu[a] = new Student();
            stu[a].number = a + 1;
            // 年级1到6
-           stu[a].state = (int)(Math.random() * 10 - 2);
+           stu[a].state = (int)(Math.random() * (6 - 1 + 1) + 1);
            // 成绩
-           stu[a].score = (int)(Math.random() * 10 + 10);
-       }for (int a = 0; a < stu.length; a++) {
+           stu[a].score = (int)(Math.random() * (100 - 0 + 1));
+       }// 遍历
+       for (int a = 0; a < stu.length; a++) {
             System.out.println(stu[a].infor() ); // 遍历数组
             // System.out.println(stu[1]); // DAY08.Student@4554617c  引用类型要不存null 要不是地址
             }
         System.out.println("**************");
-        for (int a = 0; a < stu.length; a++){
+        for (int a = 0; a < stu.length; a++) {
            if (stu[a].state == 3) {
                System.out.println("成绩为:" + stu[a].score + "\t年级:" + stu[a].state + "\t序号为" + stu[a].number);// 遍历
            }
+        }
+        System.out.println("************");
+        for (int a = 0; a < stu.length - 1; a++) {
+            for (int j = 0; j < stu.length - 1 - a; j++) {
+                if (stu[j].score > stu[j + 1].score) {
+                    Student num = stu[j]; // 如果要换顺序,换的不只是成绩
+                    stu[j] = stu[j + 1];
+                    stu[j + 1] = num;
+                }
+            }
+        }
+        for (int c = 0; c < stu.length; c ++) {
+            System.out.println(stu[c].infor());
         }
     }
 }
@@ -36,17 +48,5 @@ class Student {
     int score;
     public String infor () {
         return  "成绩为:" + score + "\t年级:" + state + "\t序号为" + number;
-    }
-    public void sort (int[] num) {// 冒泡排序
-        for (int b = 0; b < num.length; b++) {
-            for (int a = 0; a < num.length - 1 - b; a++) {
-                if (num[a] > num[a+1]) {
-                    int m = num[a+1];
-                    num[a + 1] = num[a];
-                    num[a] = m;
-                }
-            }
-        }
-        System.out.println(Arrays.toString(num));
     }
 }
