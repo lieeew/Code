4478baa9e5c8e5816ef744638c9ef86203b7c0d9
lieeew <3530464951@qq.com>
2022-08-13 21:40:30 +0800

    java

---
+++ IDEA  project/SGG/com.sbg/Day09/ValueTransferTest02.java
@@ -0,0 +1,31 @@
+package Day09;
+
+/**
+ * @Auther: qiuxinyu
+ * @Date: 2022/8/13 - 08 - 13
+ * @Description: Day09
+ */
+public class ValueTransferTest02 {
+    public static void main(String[] args) {
+        Date w = new Date();
+        w.m = 10;
+        w.n = 20;
+        System.out.println(w.m + "    "+ w.n );
+        ValueTransferTest02 x = new ValueTransferTest02();
+        x.change(w);
+
+        System.out.println(w.m +"    " +  w.n);
+
+    }
+    public void change(Date a) {
+        int temp = a.m;
+        a.m = a.n;
+        a.n = temp;
+    }
+}
+class Date {
+    int m ;
+    int n ;
+
+
+}
