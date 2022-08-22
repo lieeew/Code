12933011f78118b12e422907f253ea9beaae74e2
lieeew <3530464951@qq.com>
2022-08-22 18:27:10 +0800

    java

--- IDEA  project/myProject/src/com/atguigu/p2/bean/Customer.java
+++ IDEA  project/myProject/src/com/atguigu/p2/bean/Customer.java
@@ -6,4 +6,41 @@ package com.atguigu.p2.bean;
  * @Description: com.atguigu.p2.bean
  */
 public class Customer {
+    private String name;
+    private int age;
+    private String sex;
+    private String phone;
+    private String email;
+    public void setName (String name) {
+        this.name = name;
+    }
+    public String getName () {
+        return name;
+    }
+    public void setAge (int age) {
+        this.age = age;
+    }
+    public int getAge () {
+        return age;
+    }
+    public void setSex (String sex) {
+        this.sex = sex;
+    }
+    public String getSex () {
+        return sex;
+    }
+    public void setPhone (String phone) {
+        this.phone = phone;
+    }
+    public String getPhone () {
+        return phone;
+    }
+    public void setEmail (String email) {
+        this.email = email;
+    }
+    public String getEmail () {
+        return email;
+    }
+
+
 }
---
+++ IDEA  project/myProject/src/com/atguigu/p2/ui/CustomerView.java
@@ -0,0 +1,9 @@
+package com.atguigu.p2.ui;
+
+/**
+ * @Auther: qiuxinyu
+ * @Date: 2022/8/22 - 08 - 22
+ * @Description: com.atguigu.p2.ui
+ */
+public class CustomerView {
+}
