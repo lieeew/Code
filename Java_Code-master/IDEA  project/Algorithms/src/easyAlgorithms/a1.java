914de30e2c7ea864b680b3fd6802d0680450a2cf
lieeew <3530464851@qq.com>
2022-10-31 17:19:59 +0800

    java

---
+++ Java_Code-master/IDEA  project/Algorithms/src/easyAlgorithms/a1.java
@@ -0,0 +1,18 @@
+package easyAlgorithms;
+
+import org.junit.Test;
+
+/**
+ * @Auther: QiuXinYu
+ * @Date: 2022/10/31 - 10 - 31
+ * @Description: easyAlgorithms
+ */
+public class a1 {
+
+    @Test
+    public void test1() {
+        
+    }
+
+
+}
