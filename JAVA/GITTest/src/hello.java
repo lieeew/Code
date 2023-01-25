9b36288d95f6de1a86e6d2c30cd0e295a74ec897
leikooo <liangzilixue123456@outlook.com>
2023-01-25 21:13:25 +0800

    first commit

---
+++ JAVA/GITTest/src/hello.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: qxy
+ * @Date: 2023/1/25 21
+ * @Description:
+ */
+public class hello {
+    public static void main(String[] args) {
+
+        System.out.println("你好");
+    }
+
+}
