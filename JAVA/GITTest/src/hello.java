1f0dfef0107982d0e000deede0f5fdb4a11294c7
leikooo <liangzilixue123456@outlook.com>
2023-01-25 21:14:33 +0800

    second commit

--- JAVA/GITTest/src/hello.java
+++ JAVA/GITTest/src/hello.java
@@ -1,3 +1,5 @@
+import com.sun.corba.se.impl.orbutil.ObjectUtility;
+
 /**
  * @Author: qxy
  * @Date: 2023/1/25 21
@@ -7,6 +9,7 @@ public class hello {
     public static void main(String[] args) {
 
         System.out.println("你好");
+        System.out.println("你好啊！");
     }
 
 }
