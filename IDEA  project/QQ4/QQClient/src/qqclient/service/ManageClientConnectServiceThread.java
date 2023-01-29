24eac6bdc4920645da7f61b2edcca3ad503b5c05
leikooo <108205879+lieeew@users.noreply.github.com>
2023-01-29 17:30:07 +0800

    qq项目

--- IDEA  project/QQ4/QQClient/src/qqclient/service/ManageClientConnectServiceThread.java
+++ IDEA  project/QQ4/QQClient/src/qqclient/service/ManageClientConnectServiceThread.java
@@ -10,7 +10,18 @@ import java.util.HashMap;
 public class ManageClientConnectServiceThread {
     private static HashMap<String, clientConnectServiceThread> hm = new HashMap<>();
 
+    /**
+     * 添加线程
+     */
     public static void addClientThread(String s, clientConnectServiceThread clientConnectServiceThread) {
         hm.put(s, clientConnectServiceThread);
+    }
+
+    /**
+     * 通过指定id获取集合
+     * @return 线程
+     */
+    public static clientConnectServiceThread getThread(String userId) {
+        return hm.get(userId);
     }
 }
--- IDEA  project/QQ4/QQClient/src/qqclient/service/userClientService.java
+++ IDEA  project/QQ4/QQClient/src/qqclient/service/userClientService.java
@@ -19,8 +19,9 @@ public class userClientService {
     private User u = new User(); // 会在外面使用到user属性
     private Socket socket;
 
-    public void checkUer(String account, String psw) {
-        u.setUserId(account);
+    public boolean checkUer(String userId, String psw) {
+        boolean isSuccess = true;
+        u.setUserId(userId);
         u.setPsw(psw);
         try {
             socket = new Socket(InetAddress.getLocalHost(), 9999);
@@ -34,15 +35,19 @@ public class userClientService {
             if (message.getMessageType().equals(MessageType.MESSAGE_LOGIN_SUCCESS)) {
                 // 登录成功, 需要一个线程持有该socket, 保持和服务器的通信
                 // 启动线程
-                new clientConnectServiceThread(socket).start();
+                clientConnectServiceThread clientConnectServiceThread = new clientConnectServiceThread(socket);
+                clientConnectServiceThread.start();
                 // 加入到线程管理的集合
-
+                ManageClientConnectServiceThread.addClientThread(userId, clientConnectServiceThread);
             } else {
-                // 登录失败
+                // 登录失败, 需要关不socket
+                socket.close();
+                isSuccess = false;
             }
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+        return isSuccess;
     }
 }
 
