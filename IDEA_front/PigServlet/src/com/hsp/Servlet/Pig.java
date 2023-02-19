4c2773ca30e2723a7e9f67a4d25bfd2d4e557bcb
leikooo <108205879+lieeew@users.noreply.github.com>
2023-02-19 16:00:33 +0800

    servlet

--- IDEA_front/PigServlet/src/com/hsp/Servlet/Pig.java
+++ IDEA_front/PigServlet/src/com/hsp/Servlet/Pig.java
@@ -12,19 +12,20 @@ import java.io.IOException;
  * @Date: 2023/2/19
  * @Description:
  */
-@WebServlet(urlPatterns = {"/pig1", "/pig2"})
+@WebServlet(urlPatterns = {"/pig1", "/pig2"}, loadOnStartup = 1)
 public class Pig extends HttpServlet {
-    private int post_num = 1;
-    private int get_num = 1;
+    private int post_num = 0;
+    private int get_num = 0;
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("第" + post_num++ + " 次调用doPost方法~~~");
+        // 我是一个oop程序员, 一定要记得方法, 没有看父接口或者父类
+        System.out.println("第" + (++post_num) + " 次调用doPost方法~~~");
         System.out.println("客户端的ip" + req.getRemoteAddr());
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("第" + get_num++ + " 次调用 doGet方法~~~~");
+        System.out.println("第" + (++get_num) + " 次调用 doGet方法~~~~");
         System.out.println("客户端的ip " + req.getRemoteAddr()); // 获取ip地址
 
     }
