6347d3458d36f6af642a6ac11f3d8db8b21c6e39
leikooo <108205879+lieeew@users.noreply.github.com>
2023-07-16 23:37:02 +0800

    leikooo
    算法模块

---
+++ yugutou-master/yugutou-master/Java/src/main/java/com/leikoooo/charpter1_link_test/level1/BasicLinkListTest.java
@@ -0,0 +1,23 @@
+package com.leikoooo.charpter1_link_test.level1;
+
+import com.yugutou.charpter1_linklist.level1.BasicLinkList;
+
+/**
+ * @author leikooo
+ * @create 2023-07-16 22:25
+ * @Package com.leikoooo.charpter1_link_test.level1
+ * @Description
+ */
+public class BasicLinkListTest {
+    static class Node {
+        final int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+
+
+}
---
+++ yugutou-master/yugutou-master/Java/src/main/java/com/leikoooo/charpter1_link_test/level1/BasicLinkTest.java
@@ -0,0 +1,48 @@
+package com.leikoooo.charpter1_link_test.level1;
+
+import com.yugutou.charpter1_linklist.level1.BasicLink;
+
+/**
+ * @author leikooo
+ * @create 2023-07-16 19:48
+ * @Package com.leikoooo.charpter1_link_test.level1
+ * @Description
+ */
+public class BasicLinkTest {
+
+    public static void main(String[] args) {
+        Node node = initLinkedList(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
+        System.out.println(node);
+    }
+
+    /**
+     * 创建一串 Node
+     */
+    public static Node initLinkedList(int[] array) {
+        // 定义一个头部节点和当前节点
+        Node head = null, cur = null;
+        for (int i = 0; i < array.length; i++) {
+            Node newNode = new Node(array[i]);
+            newNode.next = null;
+            if (i == 0) {
+                head = newNode;
+                cur = newNode;
+            } else {
+                cur.next = newNode;
+                cur = newNode;
+            }
+        }
+        return head;
+
+    }
+
+    static class Node {
+        int data;
+
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+}
