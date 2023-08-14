8f7061deb7676e5dba46307d62964af35a633b50
leikooo <108205879+lieeew@users.noreply.github.com>
2023-08-14 16:38:57 +0800

    leikooo

--- 星球项目/用户处理系统/user-center-backend/mysql/creat_usertable.sql
+++ 星球项目/用户处理系统/user-center-backend/mysql/creat_usertable.sql
@@ -18,3 +18,25 @@ create table user
     planetCode   varchar(512)                       null comment '星球编号'
 );
 
+-- 增加一个字段
+
+-- auto-generated definition
+create table tag
+(
+    id         bigint auto_increment
+        primary key,
+    tagName    varchar(255)                       null comment '标签名称',
+    userId     varchar(256)                       null comment '用户 id',
+    parentId   bigint                             null comment '父标签',
+    isParent   tinyint                            null comment '0-是父标签, 1-不是父标签',
+    createTime datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint  default 0                 null comment '是否删除',
+    constraint uniIndex_tagName
+        unique (tagName)
+)
+    comment '标签表';
+
+create index idx_userId
+    on tag (userId);
+
